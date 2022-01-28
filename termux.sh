407d7f99d69539510a05020f60a37cd2d2054b88
secrno <dukechess@riseup.net>
2022-01-28 15:23:35 +0300

    some changes on termux script

--- termux.sh
+++ termux.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# root repo and packages
 pkg install root-repo && pkg update 
-
 pkg install iproute2 gnupg vim openssh nmap arp-scan python busybox termux-services apache2 git
-
 pip install youtube-dl
 
+# storage permission
 termux-setup-storage
 
-ln -s /data/data/com.termux/files/usr/etc/ ~/etc
+# system files
+ln -s /data/data/com.termux/files/usr ~/system
